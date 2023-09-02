b94f4074bb81df0259276d4c7ff6b991e51920cd
costar011 <dnaer456@gmail.com>
2023-09-02 19:59:32 +0900

    js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ tabButtons.forEach((e) => {
     e.addEventListener('click', (e) => {
         if(! e.target.classList.contains('tab__button-active')){
             e.target.classList.add('tab__button-active');
-            e.target.nextElementSibling.style.height =
-            e.target.nextElementSibling
+            e.target.nextElementSibling.style.height = '0px';
+            e.target.nextElementSibling.classList.remove('tab__panel-active');
         }
     });
 });
